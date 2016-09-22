7235ccf9b1ea567dbf2453e278d1c9d42c608251
ericyue <hi.moonlight@gmail.com>
2016-09-22 19:27:54 +0800

    up

--- data/pb2debug.py
+++ data/pb2debug.py
@@ -6,9 +6,8 @@ tfrecords_file_name = "pb/xaa.test.csv.tfrecords"
 input_file = os.path.join(current_path, tfrecords_file_name)
 print input_file
 
-# Constrain the data to print
-max_print_number = 100
-print_number = 1
+max_ptr = 100
+ptr = 1
 
 for serialized_example in tf.python_io.tf_record_iterator(input_file):
   example = tf.train.Example()
@@ -17,11 +16,10 @@ for serialized_example in tf.python_io.tf_record_iterator(input_file):
   label = example.features.feature["label"].float_list.value
   categorical_features = example.features.feature["categorical_features"].bytes_list.value
   continuous_features = example.features.feature["continuous_features"].float_list.value
-  print("Number: {}, label: {}\ncate-features: {}\nconti-features: {}".format(print_number, label,categorical_features,continuous_features))
+  print("label: {}\ncate-features: {}\nconti-features: {}".format(label,categorical_features,continuous_features))
   print("============================")
 
-  # Return when reaching max print number
-  if print_number > max_print_number:
+  if ptr > max_ptr:
     exit()
   else:
-    print_number += 1
+    ptr += 1
